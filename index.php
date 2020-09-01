c5d7a203b591df60c9d8fe6287e753075180d5c1
Alfonso <alfof87@gmail.com>
2020-09-01 16:50:57 +0200

    php-badwords

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
  $inputString = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
- // $Slorem = "Lorem *** dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
  $wordToCens = $_GET["name"];
- $sost = str_replace("***", "ipsum", $inputString);
+ $sost = str_replace($wordToCens, "***", $inputString);
 ?>
 
 <h1>
  <?php
   echo $inputString;
   echo $_GET["name"];
-
-  // echo "<br>";
-  // echo $Slorem;
  ?>
 </h1>
