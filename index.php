20bf264179286d5dd289269483005a8699f541d4
Alfonso <alfof87@gmail.com>
2020-09-01 15:37:31 +0200

    php-badwords

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+ $lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+ // $Slorem = "Lorem *** dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+ $GET_["lorem"];
+?>
+
+<h1>
+ <?php
+  echo $lorem;
+  echo "<br>";
+  // echo $Slorem;
+ ?>
+</h1>
